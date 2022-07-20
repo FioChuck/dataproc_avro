6ed90cf58d5b2554091e48d1fc155a4dd38c1429
FiocChuck <chasf@google.com>
2022-07-20 10:39:13 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Discovery Work: Loading Avro files into BQ using Pyspark on Dataproc.',
+    author='chasf',
+    license='',
+)
